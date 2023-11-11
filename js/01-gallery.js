c48f97d8e38c13bf4de9178b12e0f4b7063e55cd
culplate <culplate@gmail.com>
2023-11-11 23:40:49 +0100

    Task 1 completed
    * Added markup via JS
    * Added fullscreen option via basicLightbox library
    * Added modal closing by Esc button function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryCont = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">    
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    }).join('');
+}
+
+function fullScreen(event) {
+    if (event.target === event.currentTarget) return;
+
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}">
+    `);
+
+    event.preventDefault();
+    instance.show();
+    document.addEventListener('keydown', evt => {
+        if (instance.visible() && evt.key === "Escape") instance.close();
+    });
+}
+
+galleryCont.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+galleryCont.addEventListener('click', fullScreen); 
